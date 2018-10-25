d7cb211dd6c4a3c585a9f9d0296d55ffb6215923
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-10-25 14:37:43 -0400

    Here are a few more pages working

--- client/docs/pages/ModalsNavPage.js
+++ client/docs/pages/ModalsNavPage.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Container, Row, Col, Jumbotron, Fa, NavLink } from "mdbreact";
+import { Container, Row, Col, Jumbotron, Fa, NavLink, Iframe } from "mdbreact";
 
 class ComponentsPage extends React.Component {
   render() {
     return (
       <Container>
         <Row>
-          <Col md="8" className="mt-3 mx-auto">
+          <Col md="12" className="mt-3 mx-auto">
             <Jumbotron>
               <h1>
-                <Fa icon="window-restore" className="grey-text mr-2" />
-                Modals
+                <Fa icon="film" className="grey-text mr-2" />
+                Tutorial 
               </h1>
+              <Iframe height={500} src="https://www.youtube.com/embed/7DG3kCDx53c" />
               <ul className="list-unstyled example-components-list">
-                <h6 className="mt-3 grey-text">FREE </h6>
+                <h6 className="mt-3 grey-text">FREE Gerbils!</h6>
                 <NavLink
                   className="list-group-item list-group-item-action"
                   to="/modals/modal"
